a9f85416f9ab41d812d0af4ced5034d84fa92f05
Shining Yang <ShiningYangYXN@163.com>
2024-04-06 09:59:36 +0800

    1

--- CrazyTranscription.py
+++ CrazyTranscription.py
@@ -1,161 +1,163 @@
+# -*- coding: utf-8 -*-
+
 #Dependencies
 from secrets import choice
 
 # Mapping
 latinGreek = {
-     'TH': 'Θ', 'Th': 'Θ', 'th': 'θ',
-     'A': 'Α', 'a': 'α', 
-     'B': 'Β', 'b': 'β', 
-     'C': 'Ξ', 'c': 'ξ', 
-     'D': 'Δ', 'd': 'δ', 
-     'E': 'Ε', 'e': 'ε', 
-     'F': 'Φ', 'f': 'φ', 
-     'G': 'Γ', 'g': 'γ', 
-     'H': 'Η', 'h': 'η', 
-     'I': 'Ι', 'i': 'ι', 
-     'J': 'Ψ', 'j': 'ψ', 
-     'K': 'Κ', 'k': 'κ', 
-     'L': 'Λ', 'l': 'λ', 
-     'M': 'Μ', 'm': 'μ', 
-     'N': 'Ν', 'n': 'ν', 
-     'O': 'Ο', 'o': 'ο', 
-     'P': 'Π', 'p': 'π', 
-     'Q': 'Κ', 'q': 'κ', 
-     'R': 'Ρ', 'r': 'ρ', 
-     'S': 'Σ', 's': 'σ', 
-     'T': 'Τ', 't': 'τ', 
-     'U': 'Υ', 'u': 'υ', 
-     'V': 'Ν', 'v': 'ν', 
-     'W': 'Ω', 'w': 'ω', 
-     'X': 'Χ', 'x': 'χ', 
-     'Y': 'Υ', 'y': 'υ', 
-     'Z': 'Ζ', 'z': 'ζ'
+    'TH': 'Θ', 'Th': 'Θ', 'th': 'θ',
+    'A': 'Α', 'a': 'α', 
+    'B': 'Β', 'b': 'β', 
+    'C': 'Ξ', 'c': 'ξ', 
+    'D': 'Δ', 'd': 'δ', 
+    'E': 'Ε', 'e': 'ε', 
+    'F': 'Φ', 'f': 'φ', 
+    'G': 'Γ', 'g': 'γ', 
+    'H': 'Η', 'h': 'η', 
+    'I': 'Ι', 'i': 'ι', 
+    'J': 'Ψ', 'j': 'ψ', 
+    'K': 'Κ', 'k': 'κ', 
+    'L': 'Λ', 'l': 'λ', 
+    'M': 'Μ', 'm': 'μ', 
+    'N': 'Ν', 'n': 'ν', 
+    'O': 'Ο', 'o': 'ο', 
+    'P': 'Π', 'p': 'π', 
+    'Q': 'Κ', 'q': 'κ', 
+    'R': 'Ρ', 'r': 'ρ', 
+    'S': 'Σ', 's': 'σ', 
+    'T': 'Τ', 't': 'τ', 
+    'U': 'Υ', 'u': 'υ', 
+    'V': 'Ν', 'v': 'ν', 
+    'W': 'Ω', 'w': 'ω', 
+    'X': 'Χ', 'x': 'χ', 
+    'Y': 'Υ', 'y': 'υ', 
+    'Z': 'Ζ', 'z': 'ζ'
 }
 
 latinCyrillic = {
-     'ZH': 'Ж','Zh': 'Ж', 'zh': 'ж',
-     'SH': 'Ш', 'Sh': 'Ш', 'sh': 'ш',
-     'CH': 'Ч', 'Ch': 'Ч', 'ch': 'ч',
-     'TS': 'Ц', 'Ts': 'Ц', 'ts': 'ц',
-     'YU': 'Ю', 'Yu': 'Ю', 'yu': 'ю',
-     'YA': 'Я', 'Ya': 'Я', 'ya': 'я',
-     'KH': 'Х', 'Kh': 'Х', 'kh': 'х',
-     'SHCH': 'Щ', 'Shch': 'Щ', 'shch': 'щ',
-     'Q': 'КУ', 'q': 'ку',
-     'A': 'А', 'a': 'а', 
-     'B': 'Б', 'b': 'б', 
-     'C': 'С', 'c': 'с', 
-     'D': 'Д', 'd': 'д', 
-     'E': 'Е', 'e': 'е', 
-     'F': 'Ф', 'f': 'ф', 
-     'G': 'Г', 'g': 'г', 
-     'H': 'Х', 'h': 'х', 
-     'I': 'И', 'i': 'и', 
-     'J': 'Й', 'j': 'й', 
-     'K': 'К', 'k': 'к', 
-     'L': 'Л', 'l': 'л', 
-     'M': 'М', 'm': 'м', 
-     'N': 'Н', 'n': 'н', 
-     'O': 'О', 'o': 'о', 
-     'P': 'П', 'p': 'п', 
-     # 'Q': 'КУ', 'q': 'ку', 
-     'R': 'Р', 'r': 'р', 
-     'S': 'С', 's': 'с', 
-     'T': 'Т', 't': 'т', 
-     'U': 'У', 'u': 'у', 
-     'V': 'В', 'v': 'в', 
-     'W': 'В', 'w': 'в',
-     'X': 'Х', 'x': 'х', 
-     'Y': 'Ы', 'y': 'ы', 
-     'Z': 'З', 'z': 'з'
+    'ZH': 'Ж','Zh': 'Ж', 'zh': 'ж',
+    'SH': 'Ш', 'Sh': 'Ш', 'sh': 'ш',
+    'CH': 'Ч', 'Ch': 'Ч', 'ch': 'ч',
+    'TS': 'Ц', 'Ts': 'Ц', 'ts': 'ц',
+    'YU': 'Ю', 'Yu': 'Ю', 'yu': 'ю',
+    'YA': 'Я', 'Ya': 'Я', 'ya': 'я',
+    'KH': 'Х', 'Kh': 'Х', 'kh': 'х',
+    'SHCH': 'Щ', 'Shch': 'Щ', 'shch': 'щ',
+    'Q': 'КУ', 'q': 'ку',
+    'A': 'А', 'a': 'а', 
+    'B': 'Б', 'b': 'б', 
+    'C': 'С', 'c': 'с', 
+    'D': 'Д', 'd': 'д', 
+    'E': 'Е', 'e': 'е', 
+    'F': 'Ф', 'f': 'ф', 
+    'G': 'Г', 'g': 'г', 
+    'H': 'Х', 'h': 'х', 
+    'I': 'И', 'i': 'и', 
+    'J': 'Й', 'j': 'й', 
+    'K': 'К', 'k': 'к', 
+    'L': 'Л', 'l': 'л', 
+    'M': 'М', 'm': 'м', 
+    'N': 'Н', 'n': 'н', 
+    'O': 'О', 'o': 'о', 
+    'P': 'П', 'p': 'п', 
+    # 'Q': 'КУ', 'q': 'ку', 
+    'R': 'Р', 'r': 'р', 
+    'S': 'С', 's': 'с', 
+    'T': 'Т', 't': 'т', 
+    'U': 'У', 'u': 'у', 
+    'V': 'В', 'v': 'в', 
+    'W': 'В', 'w': 'в',
+    'X': 'Х', 'x': 'х', 
+    'Y': 'Ы', 'y': 'ы', 
+    'Z': 'З', 'z': 'з'
 }
 
 latinArabic = {
-     'A': 'أ', 'a': 'أ',
-     'B': 'ب', 'b': 'ب',
-     'C': 'ج', 'c': 'ج',
-     'D': 'د', 'd': 'د',
-     'E': 'ه', 'e': 'ه',
-     'F': 'و', 'f': 'و',
-     'G': 'ز', 'g': 'ز',
-     'H': 'ح', 'h': 'ح',
-     'I': 'ط', 'i': 'ط',
-     'J': 'ي', 'j': 'ي',
-     'K': 'ك', 'k': 'ك',
-     'L': 'ل', 'l': 'ل',
-     'M': 'م', 'm': 'م',
-     'N': 'ن', 'n': 'ن',
-     'O': 'س', 'o': 'س',
-     'P': 'ع', 'p': 'ع',
-     'Q': 'ق', 'q': 'ق',
-     'R': 'ر', 'r': 'ر',
-     'S': 'س', 's': 'س',
-     'T': 'ت', 't': 'ت',
-     'U': 'ؤ', 'u': 'ؤ',
-     'V': 'ث', 'v': 'ث',
-     'W': 'و', 'w': 'و',
-     'X': 'خ', 'x': 'خ',
-     'Y': 'ي', 'y': 'ي',
-     'Z': 'ز', 'z': 'ز'
+    'A': 'أ', 'a': 'أ',
+    'B': 'ب', 'b': 'ب',
+    'C': 'ج', 'c': 'ج',
+    'D': 'د', 'd': 'د',
+    'E': 'ه', 'e': 'ه',
+    'F': 'و', 'f': 'و',
+    'G': 'ز', 'g': 'ز',
+    'H': 'ح', 'h': 'ح',
+    'I': 'ط', 'i': 'ط',
+    'J': 'ي', 'j': 'ي',
+    'K': 'ك', 'k': 'ك',
+    'L': 'ل', 'l': 'ل',
+    'M': 'م', 'm': 'م',
+    'N': 'ن', 'n': 'ن',
+    'O': 'س', 'o': 'س',
+    'P': 'ع', 'p': 'ع',
+    'Q': 'ق', 'q': 'ق',
+    'R': 'ر', 'r': 'ر',
+    'S': 'س', 's': 'س',
+    'T': 'ت', 't': 'ت',
+    'U': 'ؤ', 'u': 'ؤ',
+    'V': 'ث', 'v': 'ث',
+    'W': 'و', 'w': 'و',
+    'X': 'خ', 'x': 'خ',
+    'Y': 'ي', 'y': 'ي',
+    'Z': 'ز', 'z': 'ز'
 }
 
 latinHebrew = {
-     'X': 'קס', 'x': 'קס',
-     'A': 'א', 'a': 'א',
-     'B': 'ב', 'b': 'ב',
-     'C': 'ג', 'c': 'ג', 
-     'D': 'ד', 'd': 'ד',
-     'E': 'ה', 'e': 'ה',
-     'F': 'ו', 'f': 'ו', 
-     'G': 'ז', 'g': 'ז',
-     'H': 'ח', 'h': 'ח',
-     'I': 'י', 'i': 'י',
-     'J': 'י', 'j': 'י', 
-     'K': 'כ', 'k': 'כ',
-     'L': 'ל', 'l': 'ל',
-     'M': 'מ', 'm': 'מ',
-     'N': 'נ', 'n': 'נ',
-     'O': 'ו', 'o': 'ו', 
-     'P': 'פ', 'p': 'פ',
-     'Q': 'ק', 'q': 'ק', 
-     'R': 'ר', 'r': 'ר',
-     'S': 'ס', 's': 'ס',
-     'T': 'ת', 't': 'ת',
-     'U': 'ו', 'u': 'ו',
-     'V': 'ו', 'v': 'ו', 
-     'W': 'ו', 'w': 'ו',
-     # 'X': 'קס', 'x': 'קס', 
-     'Y': 'י', 'y': 'י',
-     'Z': 'ז', 'z': 'ז'
+    'X': 'קס', 'x': 'קס',
+    'A': 'א', 'a': 'א',
+    'B': 'ב', 'b': 'ב',
+    'C': 'ג', 'c': 'ג', 
+    'D': 'ד', 'd': 'ד',
+    'E': 'ה', 'e': 'ה',
+    'F': 'ו', 'f': 'ו', 
+    'G': 'ז', 'g': 'ז',
+    'H': 'ח', 'h': 'ח',
+    'I': 'י', 'i': 'י',
+    'J': 'י', 'j': 'י', 
+    'K': 'כ', 'k': 'כ',
+    'L': 'ל', 'l': 'ל',
+    'M': 'מ', 'm': 'מ',
+    'N': 'נ', 'n': 'נ',
+    'O': 'ו', 'o': 'ו', 
+    'P': 'פ', 'p': 'פ',
+    'Q': 'ק', 'q': 'ק', 
+    'R': 'ר', 'r': 'ר',
+    'S': 'ס', 's': 'ס',
+    'T': 'ת', 't': 'ת',
+    'U': 'ו', 'u': 'ו',
+    'V': 'ו', 'v': 'ו', 
+    'W': 'ו', 'w': 'ו',
+    # 'X': 'קס', 'x': 'קס', 
+    'Y': 'י', 'y': 'י',
+    'Z': 'ז', 'z': 'ז'
 }
 
 latinSanskrit = {
-     'A': 'अ', 'a': 'अ',
-     'B': 'ब', 'b': 'ब',
-     'C': 'च', 'c': 'च',
-     'D': 'द', 'd': 'द',
-     'E': 'ए', 'e': 'ए',
-     'F': 'फ', 'f': 'फ',
-     'G': 'ग', 'g': 'ग',
-     'H': 'ह', 'h': 'ह',
-     'I': 'इ', 'i': 'इ',
-     'J': 'ज', 'j': 'ज',
-     'K': 'क', 'k': 'क',
-     'L': 'ल', 'l': 'ल',
-     'M': 'म', 'm': 'म',
-     'N': 'न', 'n': 'न',
-     'O': 'ओ', 'o': 'ओ',
-     'P': 'प', 'p': 'प',
-     'Q': 'क्', 'q': 'क्',
-     'R': 'र', 'r': 'र',
-     'S': 'स', 's': 'स',
-     'T': 'त', 't': 'त',
-     'U': 'उ', 'u': 'उ',
-     'V': 'व', 'v': 'व',
-     'W': 'व', 'w': 'व',
-     'X': 'क्स', 'x': 'क्स',
-     'Y': 'य', 'y': 'य',
-     'Z': 'ज', 'z': 'ज',
+    'A': 'अ', 'a': 'अ',
+    'B': 'ब', 'b': 'ब',
+    'C': 'च', 'c': 'च',
+    'D': 'द', 'd': 'द',
+    'E': 'ए', 'e': 'ए',
+    'F': 'फ', 'f': 'फ',
+    'G': 'ग', 'g': 'ग',
+    'H': 'ह', 'h': 'ह',
+    'I': 'इ', 'i': 'इ',
+    'J': 'ज', 'j': 'ज',
+    'K': 'क', 'k': 'क',
+    'L': 'ल', 'l': 'ल',
+    'M': 'म', 'm': 'म',
+    'N': 'न', 'n': 'न',
+    'O': 'ओ', 'o': 'ओ',
+    'P': 'प', 'p': 'प',
+    'Q': 'क्', 'q': 'क्',
+    'R': 'र', 'r': 'र',
+    'S': 'स', 's': 'स',
+    'T': 'त', 't': 'त',
+    'U': 'उ', 'u': 'उ',
+    'V': 'व', 'v': 'व',
+    'W': 'व', 'w': 'व',
+    'X': 'क्स', 'x': 'क्स',
+    'Y': 'य', 'y': 'य',
+    'Z': 'ज', 'z': 'ज',
 }
 
 latinTibetan = {
@@ -190,18 +192,18 @@ latinTibetan = {
 # Class Definition
 
 class Transcriber(object):
-     Mapper={}
-     def __init__(self, mapper:dict):
-          self.Mapper=mapper
-     def Transcribe(self, text:str):
-          for i in self.Mapper:
-               text=text.replace(i, str(self.Mapper.get(i,i)))
-          return text
-     def ReverseTranscribe(self, text:str):
-          for i in self.Mapper.values():
-               while i in text:
-                    text=text.replace(i, choice([j for j in self.Mapper.keys() if self.Mapper[j]==i]),1)
-          return text
+    Mapper={}
+    def __init__(self, mapper:dict):
+        self.Mapper=mapper
+    def Transcribe(self, text:str):
+        for i in self.Mapper:
+            text=text.replace(i, str(self.Mapper.get(i,i)))
+        return text
+    def ReverseTranscribe(self, text:str):
+        for i in self.Mapper.values():
+            while i in text:
+                text=text.replace(i, choice([j for j in self.Mapper.keys() if self.Mapper[j]==i]),1)
+        return text
 
 # Define Default Transcribers
 
@@ -217,53 +219,55 @@ DefaultTranscribers=[LatinGreekTranscriber,LatinCyrillicTranscriber,LatinArabicT
 
 if __name__=="__main__":
      print("Welcome to CrazyTranscription!")
-     print("Version 0.1, made with love by Shining Yang")
+     print("Version 2.2.1.2 (Arctic Wolf), made with love by Shining Yang")
      print("Visit https://github.com/ShiningYangYXN/CrazyTranscription for project updates.")
      while True:
-          mode=''
-          print("Please select a mode:")
-          print("    [1] Transcription")
-          print("    [2] Reverse Transcription")
-          print("    [#] Exit")
-          mode=input("Mode: ")
-          if mode=='1':
-               mode=''
-               modeNames={'1':'Greek','2':'Cyrillic','3':'Arabic','4':'Hebrew','5':'Sanskrit','6':'Tibetan'}
-               while mode!='#':
-                    print("Transcription mode. Please select a target:")
-                    print("    [1] Latin to Greek")
-                    print("    [2] Latin to Cyrillic")
-                    print("    [3] Latin to Arabic")
-                    print("    [4] Latin to Hebrew")
-                    print("    [5] Latin to Sanskrit")
-                    print("    [6] Latin to Tibetan")
-                    print("    [#] Exit")
-                    mode=input("Mode: ")
-                    if mode in ['1','2','3','4','5','6']:
-                         selectedTranscriber=DefaultTranscribers[int(mode)-1]
-                         text=''
-                         print(f'Latin to {modeNames[mode]} mode selected. Type \'#\' to exit.')
-                         while text!='#':
-                             text=input("Input your text to be transcribed:\n")
-                             print(f'---BEGIN {modeNames[mode].upper()} TRANSCRIPTION---')
-                             print(selectedTranscriber.Transcribe(text))
-                             print(f'---END {modeNames[mode].upper()} TRANSCRIPTION---')
-                    elif mode=='#':
-                         pass
-                    else:
-                         print("Invalid input. Please try again.")
+        mode=''
+        print("Please select a mode:")
+        print("    [1] Transcription")
+        print("    [2] Reverse Transcription")
+        print("    [#] Exit")
+        mode=input("Mode: ")
+        if mode=='1':
+            mode=''
+            modeNames={'1':'Greek','2':'Cyrillic','3':'Arabic','4':'Hebrew','5':'Sanskrit','6':'Tibetan'}
+            while mode!='#':
+                print("Transcription mode. Please select a target:")
+                print("    [1] Latin to Greek")
+                print("    [2] Latin to Cyrillic")
+                print("    [3] Latin to Arabic")
+                print("    [4] Latin to Hebrew")
+                print("    [5] Latin to Sanskrit")
+                print("    [6] Latin to Tibetan")
+                print("    [#] Exit")
+                mode=input("Mode: ")
+                if mode in ['1','2','3','4','5','6']:
+                    selectedTranscriber=DefaultTranscribers[int(mode)-1]
+                    text=''
+                    print(f'Latin to {modeNames[mode]} Mode selected. Type \'#\' to exit.')
+                    while text!='#':
+                        text=input("Input your text to be transcribed:\n")
+                        print(f'---BEGIN {modeNames[mode].upper()} TRANSCRIPTION---')
+                        print(selectedTranscriber.Transcribe(text))
+                        print(f'---END {modeNames[mode].upper()} TRANSCRIPTION---')
+                    print("OK,thanks. Goodbye.")
+                elif mode=='#':
+                    print("OK,thanks. Goodbye.")
+                else:
+                    print("Invalid input. Please try again.")
 
-          elif mode=='2':
-               print("Reverse Transcription Mode. Type\'#\' to exit.")
-               text=''
-               while text!='#':
-                    text=input("Input your text to be reverse transcribed:\n")
-                    print("---BEGIN REVERSE TRANSCRIPTION---")
-                    for t in DefaultTranscribers:
-                        text=t.ReverseTranscribe(text)
-                    print(text)
-                    print("---END REVERSE TRANSCRIPTION---")
-          elif mode=='#':
-               break
-          else:
-               print("Invalid input. Please try again.")
+        elif mode=='2':
+            print("Reverse Transcription Mode. Type\'#\' to exit.")
+            text=''
+            while text!='#':
+                text=input("Input your text to be reverse transcribed:\n")
+                print("---BEGIN REVERSE TRANSCRIPTION---")
+                for t in DefaultTranscribers:
+                    text=t.ReverseTranscribe(text)
+                print(text)
+                print("---END REVERSE TRANSCRIPTION---")
+        elif mode=='#':
+            print("OK,thanks. Goodbye.")
+            break
+        else:
+            print("Invalid input. Please try again.")
